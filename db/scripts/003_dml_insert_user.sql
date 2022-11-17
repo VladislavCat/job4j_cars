8848b13103dcb0435c5693a9c04a981adce5a063
VladislavCat <kshift84@gmail.com>
2022-11-17 20:17:21 +0300

    2. HQL [#6874]

---
+++ db/scripts/003_dml_insert_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO auto_user (username, password) VALUES ('Ivanov', 'root');
+INSERT INTO auto_user (username, password) VALUES ('Petrov', 'root');
+INSERT INTO auto_user (username, password) VALUES ('Sidorov', 'root');
