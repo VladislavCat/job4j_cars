968c3b9d2b854f42d3586ca14a3ecf28760ea56b
VladislavCat <kshift84@gmail.com>
2022-11-30 17:17:10 +0300

    1. ToMany [#301848]

---
+++ src/main/java/model/PriceHistory.java
@@ -0,0 +1,23 @@
+package model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "PRICE_HISTORY")
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class PriceHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    @NonNull
+    private int before;
+    @NonNull
+    private int after;
+    private LocalDateTime ldt;
+}
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "auto_user")
@@ -15,4 +16,7 @@ public class User {
     private int id;
     private String username;
     private String password;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "auto_post_id")
+    private List<PriceHistory> priceHistoryList;
 }
