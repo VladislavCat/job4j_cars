efe798c88e390ea2042affac64e645596bb860f4
VladislavCat <kshift84@gmail.com>
2022-12-15 18:34:02 +0300

    Модели и связи. Машины и владельцы [#4744]

--- src/main/java/model/Driver.java
+++ src/main/java/model/Driver.java
@@ -10,7 +10,6 @@ import javax.persistence.*;
 @Entity
 @Table(name = "drivers")
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class Driver {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
