17e02ec5881d068dcee5d8b9031d6d541563e3d1
VladislavCat <kshift84@gmail.com>
2023-01-11 18:41:03 +0300

    Создание слоя контроллеров и вспомогательных методов

---
+++ src/main/java/cars/controller/PostController.java
@@ -0,0 +1,80 @@
+package cars.controller;
+
+import cars.util.UserName;
+import lombok.RequiredArgsConstructor;
+import cars.model.Post;
+import cars.model.User;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import cars.service.CarService;
+import cars.service.PostService;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+    private final cars.service.PostService postService;
+    private final CarService carService;
+
+    @GetMapping("/all_posts")
+    public String all(Model model, HttpSession httpSession) {
+        UserName.userSessionSetName(model, httpSession);
+        model.addAttribute("posts", postService.findAll());
+        return "/all_posts";
+    }
+
+    @GetMapping("/last_day_posts")
+    public String lastDay(Model model, HttpSession httpSession) {
+        UserName.userSessionSetName(model, httpSession);
+        model.addAttribute("posts", postService.findLastDay());
+        return "/last_day_posts";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model, HttpSession httpSession) {
+        UserName.userSessionSetName(model, httpSession);
+        model.addAttribute("post", new Post());
+        model.addAttribute("cars", carService.findAll());
+        return "/create";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute Post post, HttpSession httpSession,
+                      @RequestParam("file") MultipartFile file) throws IOException {
+        if (file != null) {
+            post.setPhoto(file.getBytes());
+        }
+        post.setCreated(LocalDateTime.now());
+        post.setUser((User) httpSession.getAttribute("user"));
+        post.setStatus(true);
+        postService.addPost(post);
+        return "redirect:/all_posts";
+    }
+
+    @GetMapping("/detail/{postId}")
+    public String detail(@PathVariable("postId") int id, Model model, HttpSession httpSession) {
+        UserName.userSessionSetName(model, httpSession);
+        model.addAttribute("post", postService.findById(id));
+        return "detail";
+    }
+
+    @GetMapping("/photoPost/{postId}")
+    public ResponseEntity<Resource> download(@PathVariable("postId") Integer sessionId) {
+        Post post = postService.findById(sessionId);
+        return ResponseEntity.ok()
+                .headers(new HttpHeaders())
+                .contentLength(post.getPhoto().length)
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new ByteArrayResource(post.getPhoto()));
+    }
+}
---
+++ src/main/java/cars/controller/UserController.java
@@ -0,0 +1,68 @@
+package cars.controller;
+
+import lombok.AllArgsConstructor;
+import cars.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import cars.service.UserService;
+import cars.util.UserName;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/loginPage")
+    public String loginPage(Model model, @RequestParam(name = "fail", required = false) Boolean fail) {
+        model.addAttribute("fail", fail != null);
+        return "/login_page";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute User user, HttpServletRequest req) {
+        Optional<User> userDb = userService.findUserByUsernameAndPassword(
+                user.getUsername(), user.getPassword()
+        );
+        if (userDb.isEmpty()) {
+            return "redirect:/loginPage?fail=true";
+        }
+        HttpSession session = req.getSession();
+        session.setAttribute("user", userDb.get());
+        return "redirect:/all_posts";
+    }
+
+    @GetMapping("/formRegistration")
+    public String createUser(Model model, @RequestParam(name = "fail", required = false) Boolean fail) {
+        model.addAttribute("user", new User("username", "password"));
+        model.addAttribute("fail", fail != null);
+        return "formRegistration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(Model model, @ModelAttribute User user) {
+        Optional<User> regUser = Optional.ofNullable(userService.add(user));
+        if (regUser.isEmpty()) {
+            model.addAttribute("message", "Пользователь с таким именем уже существует");
+            return "redirect:/formRegistration?fail=true";
+        }
+        return "redirect:/all_tasks";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login_page";
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model, HttpSession session) {
+        UserName.userSessionSetName(model, session);
+        return "profile";
+    }
+
+}
